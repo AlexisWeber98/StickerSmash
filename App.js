1133c12ceaf3d667e1cc22e05f721d8743b5fd4c
Alexis Weber <Alexis.Weber@mailfence.com>
2023-09-27 22:42:45 -0300

    button pick image function

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 //--------------------components----------------------//
 
@@ -9,6 +10,19 @@ import Button from "./components/Button";
 export default function App() {
   const placeHolderImage = require("./assets/images/background-image.png");
 
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert("You did not  selected any image");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -16,7 +30,11 @@ export default function App() {
       </View>
 
       <View style={styles.footerContainer}>
-        <Button theme= "primary" label="Choose a photo" />
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
         <Button label="Use this photo" />
       </View>
       <StatusBar style="auto" />
