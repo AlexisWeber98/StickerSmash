44036c72137dbc97748e84f3f177f8d818850b42
Alexis Weber <Alexis.Weber@mailfence.com>
2023-10-01 18:29:30 -0300

    reinstall gesture handler and config babelconfig.js

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,8 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      "@bablel/plugin-proposal-export-namespace-from",
-      "react-native-reanimated-plugin",
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
     ],
   };
 };
