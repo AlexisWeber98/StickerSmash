2b82d40434f90d0578c46fbcde01d03dc1117d64
Alexis Weber <Alexis.Weber@mailfence.com>
2023-09-30 21:31:15 -0300

    emojiSticker component

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,3 +1,9 @@
-export default EmojiSticker = () => {
-    
+import { View, Image } from "react-native"
+
+export default EmojiSticker = ( {imageSize, stickerSource}) => {
+return(
+    <View style={{top:-350}}>
+        <Image source={stickerSource} resizeMode="contain" style={{width: imageSize, height: imageSize}}/>
+    </View>
+)
 }
