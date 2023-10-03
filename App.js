b31224616b553639849c355d80b680d93d580a30
Alexis Weber <Alexis.Weber@mailfence.com>
2023-10-03 19:11:00 -0300

    change statusBar (ligth to dark)

--- App.js
+++ App.js
@@ -134,7 +134,7 @@ export default function App() {
           <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
         </EmojiPicker>
       </View>
-      <StatusBar style="light" />
+      <StatusBar style="dark" />
     </GestureHandlerRootView>
   );
 }
